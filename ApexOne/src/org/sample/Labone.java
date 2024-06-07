14ec3b35c8997c6a4a180fb6ae35f27fa841be48
devmanagement23 <devmanagement23@gmail.com>
2024-06-07 16:49:45 +0530

    1st commit

---
+++ ApexOne/src/org/sample/Labone.java
@@ -0,0 +1,11 @@
+package org.sample;
+
+public class Labone {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello from Apex Lab three");
+
+	}
+
+}
